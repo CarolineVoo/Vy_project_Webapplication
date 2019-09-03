7c74a2d6539450f2ba7ed595a7cfa1c06ab6e033
PiotrKusnierz <piotr.kusnierz@interia.eu>
2019-09-03 11:54:28 +0200

    Added HomeController

---
+++ Gruppeoppgave 1/Gruppeoppgave 1/App_Start/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gruppeoppgave_1.App_Start
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
