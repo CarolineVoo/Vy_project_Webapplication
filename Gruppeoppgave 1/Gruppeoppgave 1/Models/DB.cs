bb2b60c6741c3cc742b4150d195d644eade9476a
PiotrKusnierz <piotr.kusnierz@interia.eu>
2019-09-04 13:42:53 +0200

    Updated a basic Db class (I hope)

--- Gruppeoppgave 1/Gruppeoppgave 1/Models/DB.cs
+++ Gruppeoppgave 1/Gruppeoppgave 1/Models/DB.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
 namespace Gruppeoppgave_1.Models
 {
-    public class DB
+    public class DB : DbContext
     {
+        public DB()
+            : base("name=Bilett")
+        {
+            Database.CreateIfNotExists();
+        }
+        public DbSet<Kunde> Kunder { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
+
+    public class Kunde
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+    }
+
 }
